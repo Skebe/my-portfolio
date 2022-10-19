cf799a032167c9ea9e718c543900e54294ac4082
Skebe <kebeserigne099@gmail.com>
2022-10-19 21:29:33 +0200

    added my picture + last commit

--- src/screens/aboutMe/AboutMe.jsx
+++ src/screens/aboutMe/AboutMe.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./aboutMe.css";
-//import person from "../../images/img-srk.jpg";
-import person from "../../images/person.png";
+import person from "../../images/img-srk.jpg";
+//import person from "../../images/person.png";
 import {welcomeMsg} from "../../helpers/parcours";
 import {presentation} from "../../helpers/parcours";
 
@@ -9,7 +9,7 @@ function AboutMe() {
     return (
         <div className="about_container" id="about-me">
             <div>
-                <img src={person} alt="person icon"/>
+                <img src={person} alt="person icon" style={{width: '250px', height: '250px'}}/>
             </div>
             <div className="about_text">
                 <h1>A PROPOS DE MOI</h1>
