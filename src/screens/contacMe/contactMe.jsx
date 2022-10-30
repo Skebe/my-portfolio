3fc7a0b5a0eca5d4060bbcc802ad2f3de543970c
Skebe <kebeserigne099@gmail.com>
2022-10-30 13:45:11 +0100

    added icons msg

--- src/screens/contacMe/contactMe.jsx
+++ src/screens/contacMe/contactMe.jsx
@@ -5,6 +5,7 @@ import {FaLinkedin} from "react-icons/fa";
 import {SiGmail} from "@react-icons/all-files/si/SiGmail";
 import {FaTwitter} from "react-icons/fa";
 import {SiGithub} from "@react-icons/all-files/si/SiGithub";
+import {GrMail} from "@react-icons/all-files/gr/GrMail";
 
 function createLinks() {
     return navigationLinks.map((e, index) => (
@@ -20,7 +21,7 @@ function ContactMe() {
                     <p><a href="https://linkedin.com/in/serigne-kebe-83ba26177" target="_blank"><FaLinkedin/>&nbsp;LinkedIn</a></p>
                     <p><a href="https://twitter.com/KEBESerigne2" target="_blank"><FaTwitter/>&nbsp;Twitter</a></p>
                     <p><a href="https://github.com/Skebe/" target="_blank"><SiGithub/>&nbsp;Github</a></p>
-                    <p><a href="mailto:kebeserigne099@gmail.com" target="_blank"><SiGmail/>&nbsp;kebeserigne099@gmail.com</a></p>
+                    <p><a href="mailto:kebeserigne099@gmail.com" target="_blank"><GrMail/>&nbsp;kebeserigne099@gmail.com</a></p>
                 </div>
                 <div>
                     {createLinks()}
